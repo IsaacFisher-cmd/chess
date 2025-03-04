4b64938d0b35692e2290c73c623d247555b35288
Isaac Fisher <ironisaacfisher@gmail.com>
2025-03-03 23:01:23 -0700

    Apparently the Server and data access exception files were not going to git so adding those now

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,90 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.*;
+import service.GameService;
+import service.UserService;
+import spark.*;
+
+public class Server {
+
+
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    GameDAO gameDAO;
+
+    UserService userService;
+    GameService gameService;
+
+    UserHandler userHandler;
+    GameHandler gameHandler;
+
+    public Server() {
+
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(gameDAO, authDAO);
+
+        userHandler = new UserHandler(userService);
+        gameHandler = new GameHandler(gameService);
+
+    }
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+
+        // Each of our endpoints
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+
+        Spark.exception(BadRequestException.class, this::badRequestExceptionHandler);
+        Spark.exception(UnauthorizedException.class, this::unauthorizedExceptionHandler);
+        Spark.exception(Exception.class, this::genericExceptionHandler);
+
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+    // Methods needed for endpoints
+    private Object clear(Request req, Response resp) {
+        userService.clear();
+        gameService.clear();
+
+            resp.status(200);
+            return "{}";
+    }
+
+    private void badRequestExceptionHandler(BadRequestException ex, Request req, Response resp) {
+        resp.status(400);
+        resp.body("{ \"message\": \"Error: bad request\" }");
+    }
+
+    private void unauthorizedExceptionHandler(UnauthorizedException ex, Request req, Response resp) {
+        resp.status(401);
+        resp.body("{ \"message\": \"Error: unauthorized\" }");
+    }
+
+    private void genericExceptionHandler(Exception ex, Request req, Response resp) {
+        resp.status(500);
+        resp.body("{ \"message\": \"Error: %s\" }".formatted(ex.getMessage()));
+    }
+
+}
