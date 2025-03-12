ceca7a8d6c208c16cb7f694e3b409cf251af986e
Isaac Fisher <ironisaacfisher@gmail.com>
2025-03-11 23:07:45 -0600

    Transferring userDAO over to sql first

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,5 @@
 package server;
 
-import com.google.gson.Gson;
 import dataaccess.*;
 import service.GameService;
 import service.UserService;
@@ -21,7 +20,7 @@ public class Server {
 
     public Server() {
 
-        userDAO = new MemoryUserDAO();
+        userDAO = new SQLUserDAO();
         authDAO = new MemoryAuthDAO();
         gameDAO = new MemoryGameDAO();
 
