cca49b6c6597340c29c0ac6b892c3f0230370632
Isaac Fisher <ironisaacfisher@gmail.com>
2025-03-24 13:44:52 -0600

    starting up a server facade file

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade server = new ServerFacade();
+
+        System.out.println(server.register("username", "password", "email"));
+
+        System.out.println(server.login("username", "password"));
     }
 }
