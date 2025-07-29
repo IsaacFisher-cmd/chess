72dafc596794d7d6edbb4d0f24040d64f45557a8
Isaac Fisher <ironisaacfisher@gmail.com>
2025-07-28 22:18:48 -0600

    create server facade location

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,18 @@
+package ui;
+
+import model.GameData;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+}
