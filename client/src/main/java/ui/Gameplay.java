6586e590004213d58cc23c8548d8adc9858f7bc9
Isaac Fisher <ironisaacfisher@gmail.com>
2025-07-29 17:28:29 -0600

    board printer

--- client/src/main/java/ui/Gameplay.java
+++ client/src/main/java/ui/Gameplay.java
@@ -4,6 +4,8 @@ import server.ServerFacade;
 
 import java.util.Scanner;
 
+import static ui.EscapeSequences.*;
+
 public class Gameplay {
     private ServerFacade server;
 
@@ -17,6 +19,32 @@ public class Gameplay {
     }
 
     public void printBoard(){
+        System.out.print(ERASE_SCREEN);
 
+        String[][] board = {
+                {BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK},
+                {BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN},
+                {WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK},
+        };
+
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                String color;
+
+                if ((i + j) % 2 == 0){
+                    color = SET_BG_COLOR_MAGENTA;
+                } else {
+                    color = SET_BG_COLOR_DARK_GREEN;
+                }
+
+                System.out.print(color + board[i][j]);
+            }
+            System.out.print(RESET_BG_COLOR + "\n");
+        }
     }
 }
