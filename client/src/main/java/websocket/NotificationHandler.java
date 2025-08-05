32681efce298372dc0541b969aad8b2ed24efb74
Isaac Fisher <ironisaacfisher@gmail.com>
2025-08-05 16:59:16 -0600

    building the framework

---
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -0,0 +1,11 @@
+package websocket;
+
+import websocket.messages.*;
+
+public interface NotificationHandler {
+    void notify(NotificationMessage notification);
+
+    void notify(NotificationMessage notification);
+
+    void notify(NotificationMessage notification);
+}
